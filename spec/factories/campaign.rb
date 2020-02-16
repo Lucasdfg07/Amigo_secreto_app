abf8f363b5e208651f4d31e09fd04eee4a50dc72
Lucas Siqueira Fernandes <lucassiqueirafernandes07@gmail.com>
2020-02-16 12:07:47 -0300

    Adjusting campaign

--- spec/factories/campaign.rb
+++ spec/factories/campaign.rb
@@ -1,7 +1,11 @@
 FactoryBot.define do
     factory :campaign do
-        title         { FFaker::Lorem.word }
-        description   { FFaker::Lorem.sentence }
-        user
+      title         { FFaker::Lorem.word }
+      description   { FFaker::Lorem.sentence }
+      user
+      status        { :pending }
+      locate        { "#{FFaker::Address.city}, #{FFaker::Address.street_address}"}
+      event_date    { FFaker::Time.date }
+      event_hour    { rand(24).to_s }
     end
-end
+   end
