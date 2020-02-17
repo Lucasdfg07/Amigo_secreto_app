b568db3f3a460264ec43eaf7c6dfe240518de6bf
Lucas Siqueira Fernandes <lucassiqueirafernandes07@gmail.com>
2020-02-17 15:05:09 -0300

    Configuring production on NGINX but have some sets loading and creating/migrating production database

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,8 +72,10 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
 #  config.action_mailer.default_url_options = { :host => request.host_with_port }
-  config.action_mailer.delivery_method = :smtp
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'http://localhost:3000' }
+  
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
     address:              ENV["SMTP_ADDRESS"],
