5d2641def4f1b64a9a1cc544892eebb66e72ccb0
Lucas Siqueira Fernandes <lucassiqueirafernandes07@gmail.com>
2020-02-18 14:18:37 -0300

    trying to fix assets error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,17 +16,14 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  
-  # config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-  config.public_file_server.enabled = true
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
-  config.assets.precompile = [‘*.js’, ‘*.scss’, ‘*.css’ ‘*.css.erb’]
+  config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -73,8 +70,6 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-
-#  config.action_mailer.default_url_options = { :host => request.host_with_port }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => 'http://localhost:3000' }
