8c034c625af8fdf878fc3abb14ed81721292df81
Lucas Siqueira Fernandes <lucassiqueirafernandes07@gmail.com>
2020-02-19 16:26:27 -0300

    Final project without assets and with assets.precompile set true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,9 +71,9 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+#  config.action_mailer.default_url_options = { :host => request.host_with_port }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'http://localhost:3000' }
-  
+
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
     address:              ENV["SMTP_ADDRESS"],
